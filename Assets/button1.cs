b79601e11941233a95cb6ae468b99b034ad7cc02
nadja cvetkovic <nadjacvetkovic0@gmail.com>
2023-10-27 00:16:24 +0200

    ucinak2

--- Assets/button1.cs
+++ Assets/button1.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class button1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene("pocetna");
     }
 
     // Update is called once per frame
     void Update()
     {
+            SceneManager.LoadScene("scena2");
         
     }
 }
